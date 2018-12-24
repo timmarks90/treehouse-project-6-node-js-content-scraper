eda37a3930dd989b77c494658a31a0782b0c12a7
Tim Marks <timmarks90@gmail.com>
2018-12-24 11:07:44 -0800

    Bug fixes and updated promise for csv writer

--- scraper.js
+++ scraper.js
@@ -21,6 +21,7 @@ today = year + '-' + month + '-' + day;
 // Visit main website url
 const entryUrl = 'http://shirts4mike.com/shirts.php';
 const shirtRequest = () => {
+    const records = [];
     request(entryUrl, (error, response, html) => {
         if (!error && response.statusCode == 200) {
             const $ = cheerio.load(html);
@@ -30,7 +31,12 @@ const shirtRequest = () => {
                 const mainUrl = 'http://shirts4mike.com';
                 // Add individual shirt links to the main url
                 const fullURL = mainUrl + '/' + shirtLink;
-                shirtPage(fullURL);
+                records.push(shirtPage(fullURL));
+            });
+            Promise.all(records)
+            .then(i => {
+                csvWriter.writeRecords(i) // returns a promise
+                .then(() => console.log('Scraping Done...'));
             });
         } else if (error) {
             console.log("There’s been a 404 error. Cannot connect to http://shirts4mike.com.");
@@ -61,52 +67,60 @@ const csvWriter = createCsvWriter({
  
 // visit the individual shirt pages
 const shirtPage = (link) => {
-    request(link, (error, response, html) => {
-        if (!error && response.statusCode == 200) {
-            //load html content
-            const $ = cheerio.load(html);
-            //get shirt price
-            const shirtPrice = $('.shirt-details .price').text();
-            // get shirt title, removing the dollar amount and the comma to place entire title in its own csv column
-            const shirtTitle = $('.shirt-details h1').children().remove().end().text().replace(/,/, '');
-            // get shirt img url
-            const shirtImgURL = $('.shirt-picture span img').attr('src');
-            // get shirt page url
-            const shirtURL = link;
-            // get current time
-            let today = new Date();
-            let hours = today.getHours();
-            let minutes = today.getMinutes();
-            // Add 0 in front of minute number if less than 10
-            if (minutes < 10) {
-                minutes = '0' + minutes;
-            }
-            // Add AM/PM format to time
-            if (hours == 12) {
-                minutes = minutes + ' PM';
-            } else if (hours == 24) {
-                hours = hours - 12;
-                minutes = minutes + ' AM';
-            }
-            else if (hours < 24 && hours > 12) {
-                hours = hours - 12;
-                minutes = minutes + ' PM';
-            } else {
-                minutes = minutes + ' AM';
-            }
-            let time = hours + ':' + minutes;
-            
-            //Write page content into CSV rows
-            // writeCSV.write(`${shirtTitle},${shirtPrice},${shirtImgURL},${shirtURL},${time} \n`);
+    return new Promise((resolve, reject) => {
+        request(link, (error, response, html) => {
+            if (!error && response.statusCode == 200) {
+                try {
+                    //load html content
+                    const $ = cheerio.load(html);
+                    //get shirt price
+                    const shirtPrice = $('.shirt-details .price').text();
+                    // get shirt title, removing the dollar amount and the comma to place entire title in its own csv column
+                    const shirtTitle = $('.shirt-details h1').children().remove().end().text().replace(/,/, '');
+                    // get shirt img url
+                    const shirtImgURL = $('.shirt-picture span img').attr('src');
+                    // get shirt page url
+                    const shirtURL = link;
+                    // get current time
+                    let today = new Date();
+                    let hours = today.getHours();
+                    let minutes = today.getMinutes();
+                    // Add 0 in front of minute number if less than 10
+                    if (minutes < 10) {
+                        minutes = '0' + minutes;
+                    }
+                    // Add AM/PM format to time
+                    if (hours == 12) {
+                        minutes = minutes + ' PM';
+                    } else if (hours == 24) {
+                        hours = hours - 12;
+                        minutes = minutes + ' AM';
+                    }
+                    else if (hours < 24 && hours > 12) {
+                        hours = hours - 12;
+                        minutes = minutes + ' PM';
+                    } else {
+                        minutes = minutes + ' AM';
+                    }
+                    let time = hours + ':' + minutes;
+                    
+                    //Write page content into CSV rows
+                    // writeCSV.write(`${shirtTitle},${shirtPrice},${shirtImgURL},${shirtURL},${time} \n`);
 
-            const records = [
-                {title: shirtTitle, price: shirtPrice, imgurl: shirtImgURL, url: shirtURL, time: time}
-            ];
-            csvWriter.writeRecords(records) // returns a promise
-            .then(() => {
-                console.log('Scraping Done...');
-            });
-        }
+                    const records = {
+                        title: shirtTitle,
+                        price: shirtPrice,
+                        imgurl: shirtImgURL,
+                        url: shirtURL,
+                        time: time
+                    };
+                    resolve(records);
+                } 
+                catch(err) {
+                    reject(err);
+                }
+            }
+        });
     });
 };
 
